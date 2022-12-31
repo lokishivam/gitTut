2d755d0ea00cc35296f7b8b1ebdbf5693e51a70a
lokishivam <lokishivam@gmail.com>
2023-01-01 00:06:19 +0530

    booking final

--- bookingapp/index.js
+++ bookingapp/index.js
@@ -4,37 +4,45 @@ const btn = document.getElementById('btn');
 const phone = document.getElementById('phone');
 const list = document.getElementById('users');
 
+
 btn.addEventListener('click', e => {
     e.preventDefault();
-    console.log(name.value, email.value) 
+    
     localStorage.setItem(email.value, JSON.stringify({name : name.value , phone : phone.value ,email : email.value}));  
     const li =  document.createElement('li');
     const button = document.createElement('button');
+    const button2 = document.createElement('button');
     button.innerText = 'delete';
     button.className = 'delete';
-    li.innerText = `${name.value} ${email.value} ${phone.value}`;
+    button2.innerText = 'edit';
+    button2.className = 'edit';
+    li.innerText = `${name.value}-${email.value}-${phone.value}`;
     li.appendChild(button);
+    li.appendChild(button2);
     list.appendChild(li);
-
-    //let users = localStorage.getItem("users"); 
-    // if(users == null){
-    //     users = [];
-    //     users.push({name : name.value , email : email.value});
-    //     localStorage.setItem("users" , JSON.stringify(users));
-    // }
-    // else{
-    //     let data = JSON.parse(users);
-    //     data.push({name : name.value , email : email.value})
-    //     localStorage.setItem("users" , JSON.stringify(data));
-    // }
+    name.value = '';
+    email.value = '';
+    phone.value = '';
     }
   );
 
 
   list.addEventListener('click', (e) => {
-    console.log(e.target);
+   
     if(e.target.classList.contains('delete')){
         const par = e.target.parentElement;
+        par.remove();
+    }
+    if(e.target.classList.contains('edit')){
+        const par = e.target.parentElement;
+        console.log(typeof par.childNodes[0].textContent);
+        let data = (par.childNodes[0].textContent).split('-')[1];
+        
+        let user = JSON.parse(localStorage.getItem(data));
+        
+        name.value = user.name;
+        email.value = user.email;
+        phone.value = user.phone;
         par.remove();
     }
   });
