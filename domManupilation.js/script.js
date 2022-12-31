3a189ce6cf1bec15694594f6fba340f777c731c0
lokishivam <lokishivam@gmail.com>
2022-12-31 17:29:40 +0530

    updated DOM p3

--- domManupilation.js/script.js
+++ domManupilation.js/script.js
@@ -5,3 +5,10 @@ mainHeader.style.borderBottom = "solid 3px #000";
 
 const title = document.getElementById("add");
 title.style.color = 'green';
+
+const lists = document.getElementsByClassName('list-group-item');
+for(var i = 0; i < lists.length; i++){
+    lists[i].style.fontWeight = "bold";
+}
+
+lists[2].style.backgroundColor = "green";
