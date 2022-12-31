614bc41083b5be2be59097b335072dc381b25fda
lokishivam <lokishivam@gmail.com>
2022-12-31 23:13:15 +0530

    bookingv part 3

--- bookingapp/index.js
+++ bookingapp/index.js
@@ -1,20 +1,26 @@
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const btn = document.getElementById('btn');
+const phone = document.getElementById('phone');
+const list = document.getElementById('users');
 
 btn.addEventListener('click', e => {
     e.preventDefault();
-    console.log(name.value, email.value)   
-    let users = localStorage.getItem("users"); 
-    if(users == null){
-        users = [];
-        users.push({name : name.value , email : email.value});
-        localStorage.setItem("users" , JSON.stringify(users));
-    }
-    else{
-        let data = JSON.parse(users);
-        data.push({name : name.value , email : email.value})
-        localStorage.setItem("users" , JSON.stringify(data));
-    }
+    console.log(name.value, email.value) 
+    localStorage.setItem(email.value, JSON.stringify({name : name.value , phone : phone.value ,email : email.value}));  
+    const li =  document.createElement('li');
+    li.innerText = `${name.value} ${email.value} ${phone.value}`;
+    list.appendChild(li);
+    //let users = localStorage.getItem("users"); 
+    // if(users == null){
+    //     users = [];
+    //     users.push({name : name.value , email : email.value});
+    //     localStorage.setItem("users" , JSON.stringify(users));
+    // }
+    // else{
+    //     let data = JSON.parse(users);
+    //     data.push({name : name.value , email : email.value})
+    //     localStorage.setItem("users" , JSON.stringify(data));
+    // }
     }
   );
