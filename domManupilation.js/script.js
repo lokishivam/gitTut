d85bc869436813b2bf16302e55901752d530b3d9
lokishivam <lokishivam@gmail.com>
2022-12-31 16:42:38 +0530

    DOM manipulation added to dom branch

---
+++ domManupilation.js/script.js
@@ -0,0 +1,7 @@
+const header = document.getElementById("header-title");
+header.innerHTML = "Title";
+const mainHeader = document.getElementById("main-header");
+mainHeader.style.borderBottom = "solid 3px #000";
+
+const title = document.getElementById("add");
+title.style.color = 'green';
